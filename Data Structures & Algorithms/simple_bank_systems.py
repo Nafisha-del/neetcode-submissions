a1ea59cac0c9dc3f95958fea28ac21bfd0ff568d
Nafisha <51881741+Nafisha-del@users.noreply.github.com>
2026-09-19 14:41:04 -0400

    Create simple_bank_systems.py

---
+++ Data Structures & Algorithms/simple_bank_systems.py
@@ -0,0 +1,39 @@
+# 2043. Simple Bank System https://leetcode.com/problems/simple-bank-system/description/
+
+class Bank:
+    def __init__(self, balance: list[int]):
+        self.balance = balance
+        self.total_accounts = len(balance)
+
+    def transfer(self, account1: int, account2: int, money: int) -> bool:
+        #Check if account number is valid
+        if 1 <= account1 <= self.total_accounts and 1 <= account2 <= self.total_accounts:
+            #Check for sufficient funds
+            if self.balance[account1 - 1] >= money:
+                self.balance[account1 - 1] -= money
+                self.balance[account2 - 1] += money
+                return True
+        return False
+
+    def deposit(self, account: int, money: int) -> bool:
+        #Check if account number is valid
+        if 1 <= account <= self.total_accounts:
+            self.balance[account - 1] += money
+            return True
+        return False
+
+    def withdraw(self, account: int, money: int) -> bool:
+        #Check if account number is valid
+        if 1 <= account <= self.total_accounts:
+            #Check for sufficient funds
+            if self.balance[account - 1] >= money:
+                self.balance[account - 1] -= money
+                return True
+        return False
+
+
+# Your Bank object will be instantiated and called as such:
+# obj = Bank(balance)
+# param_1 = obj.transfer(account1,account2,money)
+# param_2 = obj.deposit(account,money)
+# param_3 = obj.withdraw(account,money)
