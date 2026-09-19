23dccc782369f7cdc3b75a7452a62e3b8c507eb6
Nafisha <51881741+Nafisha-del@users.noreply.github.com>
2026-09-19 13:49:43 -0400

    Create Maximum_Subarray.py

---
+++ Data Structures & Algorithms/Maximum_Subarray.py
@@ -0,0 +1,14 @@
+# Leetcode problem 53: https://leetcode.com/problems/maximum-subarray/description/
+
+class Solution:
+    def maxSubArray(self, nums: list[int]) -> int:
+        maxSub = nums[0]
+        curSum = 0
+
+        for n in nums:
+            if curSum < 0:
+                curSum = 0
+            curSum += n
+            maxSub = max(maxSub, curSum)
+        return maxSub
+        
